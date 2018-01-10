b1c89a332ba9e6505a8612b378c79351f70864ee
Evan Shaw <evan@vendhq.com>
2018-01-11 08:43:28 +1300

    Catch exception; show line and column numbers

--- validate-schema.js
+++ validate-schema.js
@@ -1,9 +1,16 @@
 exports.command = 'validate-schema'
 exports.desc = 'Validate GraphQL schema'
 
+const chalk = require('chalk')
 const graphql = require('graphql')
 
 exports.handler = async (context, argv) => {
   const config = await context.getProjectConfig()
-  graphql.buildSchema(config.getSchemaSDL())
+  try {
+    graphql.buildSchema(config.getSchemaSDL())
+  } catch (error) {
+    for (loc of error.locations) {
+      console.log(chalk.red(`(${loc.line}, ${loc.column}): ${error.message}`))
+    }
+  }
 }
